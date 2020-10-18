1b8289ac5d4cef88e8de6313bb2125f2038b75e3
sofuncheung <sofuncheung@qq.com>
2020-10-18 02:27:00 +0100

    train_epoch 100

--- config.py
+++ config.py
@@ -5,16 +5,25 @@ import os
 class config(object):
     alpha = 1
     optim = 'Adam'  # Options: SGD, SGD+Momentum, Adam
-    output_file_pth = '/mnt/zfsusers/sofuncheung/shake-it/playground/Sharpness/batch_size/BS-1000'
+    output_file_pth = ('/mnt/zfsusers/sofuncheung/shake-it'
+            '/playground/Sharpness/batch_size/BS-1000/100-epochs')
     lr_decay = False
+    train_epoch = 100
+
+
+    # Data loading
     num_workers = 4
     train_batch_size = 1000
     test_batch_size = 100
     sharpness_train_batch_size = 256
+
+    binary_dataset = True # This needs to be "True" when calculating volume.
+    training_set_size = 10000
+
+
+    # Sharpness & Sensitivity
     sharpness_cons = False # Consecutive recording sharpness.
     sharpness_one_off = True # Compute sharpness at last epoch.
     sensitivity_cons = False # Recording input sensitivity for each epoch of training.
     sensitivity_one_off = False # Compute input sensitivity at the end of traning.
 
-    binary_dataset = True # This needs to be "True" when calculating volume.
-    training_set_size = 10000
