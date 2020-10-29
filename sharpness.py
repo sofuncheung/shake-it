2fbbb17c838c2ba27c031e2e9e79512854ecb771
sofuncheung <sofuncheung16@gmail.com>
2020-10-29 02:12:27 +0000

    written but untested L-BFGS-B

--- sharpness.py
+++ sharpness.py
@@ -17,6 +17,36 @@ import copy
 from scipy.optimize import minimize
 
 from config import config
+from l_bfgs_b_wrapper.obj import PyTorchObjective
+
+def prepare_decorator(dataloader, loss):
+    r'''
+    Prepare necessary materials for the new
+    'forward' method.
+
+    Return:
+        A decorator.
+
+    '''
+    for batch_idx, (inputs, targets) in enumerate(dataloader):
+        print('Initializing decorator...')
+
+    def change_forward_method(net):
+        r'''
+        This is a class decorator for pytorch Module (like net), 
+        which changes the original 'forward' method.
+        '''
+        orig_forward = net.forward
+
+        def new_forward(self):
+            outputs = orig_forward(self, inputs)
+            return -loss(outputs, targets)
+
+        net.forward = new_forward
+
+        return net
+
+    return change_forward_method
 
 
 class Sharpness(object):
@@ -32,6 +62,13 @@ class Sharpness(object):
                 dataset, batch_size=config.test_batch_size,
                 shuffle=False, num_workers=config.num_workers,
                 drop_last=True)  # Note this is not on the test set.
+        
+        self.full_batch_loader = torch.utils.data.Dataloader(
+            dataset, batch_size=len(dataset), shuffle=False,
+            num_workers=config.num_workers
+        )
+
+
         # self.optimizer = optim.SGD(net.parameters(), lr=1e-3) # Have to use vanilla SGD.
         self.device = device
 
@@ -68,6 +105,33 @@ class Sharpness(object):
                 gc.collect()
             '''
         return new_params
+
+    def prepare_decorator_obsolete(self):
+        '''
+        This method passes necessary class attributes to the 
+        true decorator 'change_forward_method(cls)'.
+
+        Return:
+            A decorator.
+
+        UPDATE: After some thinking, I figure it just not gonna work when
+        there is two 'self' within the same namespace.
+        '''
+        
+        for batch_idx, (inputs, targets) in enumerate(self.full_batch_loader):
+            outputs = net(inputs)
+            ...
+
+        def change_forward_method_obsolete(cls):
+            r'''
+            This is a pytorch Module decorator, which changes the
+            original 'forward' method.
+            '''
+            orig_forward = cls.forward
+
+            def new_forward_obsolete(self):
+                ...
+
 
     def sharpness(self, clip_eps=5e-3, max_iter_epochs=100, opt_mtd='SGD'):
         net = self.net
@@ -126,7 +190,21 @@ class Sharpness(object):
                 max_value = max(max_value, epoch_loss)
                 print('max_value: ', max_value)
                 max_value_list.append(max_value)
+
         elif opt_mtd == 'L-BFGS-B':
+            
+            @prepare_decorator(self.full_batch_loader,
+                               self.loss)
+            class Objective(net):
+                def __init__(self):
+                    super(Objective, self).__init__()
+
+            objective = Objective()
+            obj = PyTorchObjective(objective)
+            
+            results = optimize.minimize(obj.fun, obj.x0, jac=obj.jac,
+                                        options={'gtol': 1e-6, 'maxiter':10})
+            print('Results of L-BFGS-B: ', results)
 
 
         np.save(os.path.join(
