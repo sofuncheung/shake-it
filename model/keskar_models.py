9ae5cecb39130568b67833862b49fadecd972d8c
sofuncheung <sofuncheung@qq.com>
2020-10-31 23:40:00 +0000

    start writting Keskar networks

---
+++ model/keskar_models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+
+
+class C1(nn.Module):
+    '''
+    C1 network architecture in Keskar's paper
+    "ON LARGE-BATCH TRAINING FOR DEEP LEARNING:
+    GENERALIZATION GAP AND SHARP MINIMA"
+    '''
+    def __init__(self)
+
